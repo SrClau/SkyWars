f4be6805a852c367816b4307e9eaaeb39dd8439a
SrClau <69334014+SrClau@users.noreply.github.com>
2020-10-08 09:33:27 -0300

    Update Load.php

--- src/CF/Load.php
+++ src/CF/Load.php
@@ -7,7 +7,19 @@ use pocketmine\event\Listener as LT;
 
 class Load extends PB implements LT {
 
+public static $instancia;
+public static $config = [];
+public const PREFIX_SW = "§8[§bSkyWars§8]";
+public const SW_WAITING = "§8[§bWaiting§8];
+
+public function onEnable(){
 
 
+$this->getLogger()->info('Enabled SkyWars');
+$this->getLogger()->notice('Poggit: https://poggit.pmmp.io/SrClau/SkyWars, GitHub: https://github.com/SrClau/SkyWars, Discord: httpz://discord.gg/');
+}
 
+public static function SkyWars(): Load {
+return self::$instancia;
+}
 }
