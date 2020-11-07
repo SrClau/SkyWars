99841a1ce07e66b2576c74e2b7dddac19cf538ea
SrClauYT <69334014+SrClau@users.noreply.github.com>
2020-11-07 19:50:38 -0300

    New complement SkyWars :D

--- src/CF/Load.php
+++ src/CF/Load.php
@@ -1,1 +1,28 @@
+<?php
 
+namespace CF;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\{Player,Server};
+use pocketmine\utils\{Config,TextFormat as Text};
+use pocketmine\entity\{Skin,Entity};
+
+class Load extends PluginBase implements Listener {
+
+/** @array Arena (arenas.yml) **/
+public $arenas = [];
+
+/** @return Load.php **/
+public static $plugin;
+
+/** @return $plugin 
+**/
+public static function getLoad(): Load {
+return self::$plugin;
+}
+
+public function getArenas(): array {
+return $this->arenas;
+}
+}
