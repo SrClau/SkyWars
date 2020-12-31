05529361202cd7753ebca8f35a2326f3f5564eca
SyleDev <61642095+XXXEserXXX@users.noreply.github.com>
2020-12-31 16:33:24 -0300

    Updated SkyWarsCommand.php (Added Command SW)
    Agregando comando [sw]

--- src/CF/SkyWars/Commands/SkyWarsCommand.php
+++ src/CF/SkyWars/Commands/SkyWarsCommand.php
@@ -1,1 +1,13 @@
+<?php 
 
+namespace CF\SkyWars\Commands;
+
+use pocketmine\command\{PluginCommand,CommandSender};
+use pocketmine\{Player,Server};
+
+class SkyWarsCommand extends PluginCommand {
+  
+  
+  
+  
+}
