fa33b263db1593890e2b81c08c7e0560335bf136
SyleDev <61642095+XXXEserXXX@users.noreply.github.com>
2020-12-31 16:27:08 -0300

    Created Status.php (Interface STATUS) [CONSTS]

---
+++ src/CF/SkyWars/Manager/Status.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace CF\SkyWars\Manager;
+
+interface Status {
+
+public const EDITING = "editing";
+
+public const START = "start";
+
+public const GAME = "game";
+
+public const END = "end";
+
+}
+?>
