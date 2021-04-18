d45585187d8c034eea038e2b97344527ff9e2f7f
Clau <69334014+SrClau@users.noreply.github.com>
2021-04-18 17:02:03 -0300

    Update SkyWars.php

--- src/SkyWars/SkyWars.php
+++ src/SkyWars/SkyWars.php
@@ -1,1 +1,19 @@
+<?php
 
+namespace SkyWars;
+
+use pocketmine\plugin\PluginBase;
+
+class SkyWars extends PluginBase {
+
+/** @var SkyWars **/
+public static $instance;
+
+public function onEnable(): void {
+self::$instance = $this;
+}
+
+public static function getInstance(): SkyWars {
+return self::$instance ?? null;
+}
+}
